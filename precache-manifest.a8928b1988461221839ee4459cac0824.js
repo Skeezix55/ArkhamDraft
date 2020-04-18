8aef3792da9bbe23b6915110143acd514f2cc5db
Tim Bennett <tpb55@earthlink.net>
2020-04-18 15:39:14 -0400

    Updates

---
+++ precache-manifest.a8928b1988461221839ee4459cac0824.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8e3487328dcd321411e383fc6a97fe7",
+    "url": "/ArkhamDraft/index.html"
+  },
+  {
+    "revision": "0f51d5f09e29d6cc07f7",
+    "url": "/ArkhamDraft/static/css/main.a84d99df.chunk.css"
+  },
+  {
+    "revision": "0a974b91b1ea3e7248bc",
+    "url": "/ArkhamDraft/static/js/2.020adf58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ArkhamDraft/static/js/2.020adf58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f51d5f09e29d6cc07f7",
+    "url": "/ArkhamDraft/static/js/main.4249fb61.chunk.js"
+  },
+  {
+    "revision": "065df5883141349b2de5",
+    "url": "/ArkhamDraft/static/js/runtime-main.2e9ec000.js"
+  }
+]);
