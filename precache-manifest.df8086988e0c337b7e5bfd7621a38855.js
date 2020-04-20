d0431751deacdba62936b6d532b39f5dd259a2ec
Tim Bennett <tpb55@earthlink.net>
2020-04-20 13:54:10 -0400

    Attempted merge

---
+++ precache-manifest.df8086988e0c337b7e5bfd7621a38855.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbe88309c9883049b7bc6d5b84203999",
+    "url": "/ArkhamDraft/index.html"
+  },
+  {
+    "revision": "d04cba1ea13e2c6d8dd4",
+    "url": "/ArkhamDraft/static/css/main.7f4a2daa.chunk.css"
+  },
+  {
+    "revision": "0a974b91b1ea3e7248bc",
+    "url": "/ArkhamDraft/static/js/2.020adf58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ArkhamDraft/static/js/2.020adf58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d04cba1ea13e2c6d8dd4",
+    "url": "/ArkhamDraft/static/js/main.04da28e6.chunk.js"
+  },
+  {
+    "revision": "065df5883141349b2de5",
+    "url": "/ArkhamDraft/static/js/runtime-main.2e9ec000.js"
+  }
+]);
