beb1a72e5f11e649ce1adbebe280f8ce184b8175
Tim Bennett <tpb55@earthlink.net>
2020-10-28 21:48:32 -0400

    Updates

---
+++ precache-manifest.900902965ae91c10afbec7f05c2bfd47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39b2eb85c8a95aa48acdd2cb936925b2",
+    "url": "/ArkhamDraft/index.html"
+  },
+  {
+    "revision": "d663d8748f78287eee2c",
+    "url": "/ArkhamDraft/static/css/main.20525106.chunk.css"
+  },
+  {
+    "revision": "0f61a72929d870c8f3e8",
+    "url": "/ArkhamDraft/static/js/2.e880d1af.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ArkhamDraft/static/js/2.e880d1af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d663d8748f78287eee2c",
+    "url": "/ArkhamDraft/static/js/main.37bb7d46.chunk.js"
+  },
+  {
+    "revision": "c9c1d82233f3e7462bb8",
+    "url": "/ArkhamDraft/static/js/runtime-main.2389b8c3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ArkhamDraft/precache-manifest.a0baafbdc88e3d0db2292a59e3bd9404.js"
+  "/ArkhamDraft/precache-manifest.900902965ae91c10afbec7f05c2bfd47.js"
 );
 
 self.addEventListener('message', (event) => {
